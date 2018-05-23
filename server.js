035799fa66edecf6e8c2f31a499e316d3f6dd6de
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-23 10:00:18 -0500

    Setting up the server.

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+
+
+const server = express();
+
+const postData = require('/data/helpers/postDb.js');
+const tagData = require('/data/helpers/userDb.js');
+const userData = require('/data/helpers/userDb.js');
