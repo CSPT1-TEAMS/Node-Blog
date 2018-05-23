1d49ccb7ea1470496ffad40c918f8049ae79604e
JohnCoronel <john.m.coronel@gmail.com>
2018-05-22 20:44:34 -0700

    user routes

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
+const userdb = require('./data/helpers/userDb')
+const postdb = require('./data/helpers/postDb')
+const tagdb = require('./data/helpers/tagDb')
+const tagRouter = require('./tags')
+
 
 const app = express()
+app.use('/tags',tagRouter)
+app.use( bodyParser.json())
+
+app.get('/users',(req,res) => {
+    userdb.get().then(results => {
+        res.status(200).send(results)
+    }).catch(err => err)
+})
+
+app.get('/users/:id',(req,res) => {
+    userdb.get(req.params.id).then(result => {
+        res.status(200).send(result)
+    })
+})
+
+app.post('/users',(req,res)=> {
+    let name = req.body.name
+    userdb.insert({name}).then(result => {
+        res.status(200).send(result)
+    })
+})
+
+app.put('/users/:id',(req,res) => {
+    userdb.update(req.params.id,{name:req.body.name}).then((result)=> {
+        res.status(200).json({result})
+    }).catch(err => res.send(err.message))
+
+})
+
+
+
+
+
 
 
 
---
+++ tags.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router()
+// /tags
+router.get('/',(req,res) => {
+
+})
+
+module.exports = router;
